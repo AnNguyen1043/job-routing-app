d025d213414c74b64934163a6e667b8dd8076834
An Nguyen <an.ndg95@gmail.com>
2024-03-23 14:35:33 +0700

    delete unused code

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useReducer, useRef, useState } from "react"
+import React, { createContext, useState } from "react"
 import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext({
